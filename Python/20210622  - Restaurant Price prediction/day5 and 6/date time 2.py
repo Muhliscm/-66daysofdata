1f2007a19172736878070ad60de5c8d773c110ed
Muhliscm <muhliscm@gmail.com>
2021-06-30 20:55:34 +0530

    all

--- Python/20210622  - Restaurant Price prediction/day5 and 6/date time 2.py
+++ Python/20210622  - Restaurant Price prediction/day5 and 6/date time 2.py
@@ -1,45 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 29 19:15:34 2021
+from datetime  import datetime
+import time
+#timestamp = 1527051855673000000
+#dt = time.strftime("%Y-%m-%d %H:%M:%S.%f",time.localtime(int(dt_str)))
+#print(dt)
 
-@author: MUHLIS
-"""
+dt_str = "2013-03-07 05:29:47.890"
+datetime_ob = datetime.strptime(dt_str, "%Y-%m-%d %H:%M:%S.%f")
+#timestamp = datetime_ob.timestamp()
+timestamp = 1527051855.673000000
+print(timestamp)
+print(datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S.%f"))
+#print(dt_str.strftime('%B %d %Y'))
 
-import pytz
-import datetime
-
-dt = datetime.datetime(2016,7, 27, 12, 30, 45, tzinfo = pytz.UTC)
-print(dt)
-
-dt_now = datetime.datetime.now(tz= pytz.UTC)
-print(dt_now)
-
-dt_utcnow = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
-print(dt_utcnow)
-
-dt_mtn = dt_now.astimezone(pytz.timezone('US/Mountain'))
-print(dt_mtn)
-
-print()
-
-for tz in pytz.all_timezones:
-    print(tz)
-    
-
-dt_mtn = datetime.datetime.now()
-mtn_tz = pytz.timezone('US/Mountain')
-
-dt_mtn = mtn_tz.localize(dt_mtn)
-dt_east = dt_mtn.astimezone(pytz.timezone("US/Eastern"))
-
-print(dt_mtn)
-print()
-print(dt_east)
-
-print(dt_mtn.isoformat())
-
-print(dt_mtn.strftime('%B %d %Y'))
-
-dt_str = "June 29 2021"
-dt = datetime.datetime.strptime(dt_str, "%B %d %Y")
-print(dt)
+#dt = datetime.strptime(dt_str, format)
+#print(datetime.utcfromtimestamp(dt_str).strftime("%Y-%m-%d %H:%M:%S.%f"))
